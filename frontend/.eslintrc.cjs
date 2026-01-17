daa19e2d51068eaa3c09496c1cfb9bda1cd1bf93
Jedai Saboteur <paradox.inversion@gmail.com>
2026-01-17 11:25:35 -0700

    chore: update @typescript-eslint and convert per-package ESLint to CJS for tsconfigRootDir

---
+++ frontend/.eslintrc.cjs
@@ -0,0 +1,25 @@
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2022,
+    sourceType: 'module',
+    ecmaFeatures: { jsx: true },
+    project: './tsconfig.json',
+    tsconfigRootDir: __dirname,
+  },
+  plugins: ['@typescript-eslint', 'react'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
+  ],
+  settings: { react: { version: 'detect' } },
+  env: { browser: true, es2021: true },
+  rules: {
+    'react/prop-types': 'off',
+    'no-unused-vars': 'warn',
+    '@typescript-eslint/no-explicit-any': 'error',
+    '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+  },
+};
